a1feaf0f66af39941c82351e32ebb639a1fbf8a4
가은 <rkdms8228@naver.com>
2022-05-04 12:16:32 +0900

    InputStream OutputStream : file

---
+++ src/com/javaex/ex01/Ex01.java
@@ -0,0 +1,36 @@
+package com.javaex.ex01;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class Ex01 {
+
+	public static void main(String[] args) throws IOException {
+		
+		InputStream in = new FileInputStream("C:\\javaStudy\\file\\img.jpg");
+		OutputStream out = new FileOutputStream("C:\\javaStudy\\file\\byteimg.jpg");
+		
+		System.out.println("복사 시작");
+		while(true) {
+			
+			int data = in.read();
+			
+			if(data == -1) {
+				System.out.println("복사 끝"+data);
+				break;
+			}
+			
+			out.write(data);
+			
+		}
+		
+		out.close();
+		in.close();
+
+	}
+
+}
