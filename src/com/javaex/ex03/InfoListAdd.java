063a44220beeec27e0950c114a60c7df7cbf4451
가은 <rkdms8228@naver.com>
2022-05-04 18:12:49 +0900

    연습 문제

---
+++ src/com/javaex/ex03/InfoListAdd.java
@@ -0,0 +1,64 @@
+package com.javaex.ex03;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class InfoListAdd {
+
+	public static void main(String[] args) throws IOException {
+		
+		InputStream in = new FileInputStream("C:\\javaStudy\\file\\PhoneDB_원본.txt");
+		InputStreamReader isr = new InputStreamReader(in);
+		BufferedReader br = new BufferedReader(isr);
+		
+		OutputStream out = new FileOutputStream("C:\\javaStudy\\file\\PhoneDB_원본-2.txt");
+		OutputStreamWriter osw = new OutputStreamWriter(out);
+		BufferedWriter bw = new BufferedWriter(osw);
+		
+		List<Info> iArray = new ArrayList<Info>();
+		
+		while(true) {
+				
+			String str = br.readLine();
+			
+			if(str == null) {
+				break;
+			}else {
+				
+				String[] info = str.split(",");
+				iArray.add(new Info(info[0], info[1], info[2]));
+				
+			}
+			
+		}
+		
+		iArray.add(new Info("김가은", "010-8563-8228", "055-111-1111"));
+			
+			for(Info i : iArray) {
+				
+				System.out.println("이름: "+i.getName());
+				System.out.println("핸드폰: "+i.getHp());
+				System.out.println("회사: "+i.getCompany());
+				System.out.println();
+				
+				String str = i.getName()+","+i.getHp()+","+i.getCompany();
+				bw.write(str);
+				bw.newLine();
+				
+			}
+			
+		bw.close();
+		br.close();
+
+	}
+
+}
---
+++ src/com/javaex/ex03/Split.java
@@ -0,0 +1,45 @@
+package com.javaex.ex03;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+
+public class Split {
+
+	public static void main(String[] args) throws IOException {
+		
+		InputStream in = new FileInputStream("C:\\javaStudy\\file\\PhoneDB_원본.txt");
+		InputStreamReader isr = new InputStreamReader(in);
+		BufferedReader br = new BufferedReader(isr);
+		
+		OutputStream out = new FileOutputStream("C:\\javaStudy\\file\\PhoneDB_원본-1.txt");
+		OutputStreamWriter osw = new OutputStreamWriter(out);
+		BufferedWriter bw = new BufferedWriter(osw);
+		
+		while(true) {
+				
+			String str = br.readLine();
+			str.split(",");
+			
+			if(str == null) {
+				break;
+			}
+			
+			System.out.println(str);
+			bw.write(str);
+			bw.newLine();
+		
+		}
+		
+		bw.close();
+		br.close();
+
+	}
+
+}
